c20a9acb08472063352d380454036366a665980e
Tarbooj <dhruvyms@gmail.com>
2026-02-24 23:22:41 +0530

    Day2: mui import fix

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,11 @@ import ProfessionalExperience from "./Navbar/NavbarComponents/ProfessionalExperi
 import NonAcademics from "./Navbar/NavbarComponents/NonAcademics";
 import MinorProjects from "./Navbar/NavbarComponents/MinorProjects";
 
-import Tab from "@mui/material";
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import Tab from '@mui/material/Tab'; // The individual Tab component still comes from @mui/material
+
 
 function App() {
   const [value, setValue] = React.useState("1");
