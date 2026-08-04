d2d95f18480f8f9be26e6cc617e343629d0a4545
Tarbooj <dhruvyms@gmail.com>
2026-08-04 23:43:27 +0530

    Ticket tracker project

--- src/CommonComponents/Seperator/CommonData/MinorProjectData.js
+++ src/CommonComponents/Seperator/CommonData/MinorProjectData.js
@@ -1,5 +1,6 @@
 import dogpic from "../../../assets/golden-retriever-dog-breed.jpg";
 import moviedux from "../../../assets/moviedux.png";
+import tickettracker from "../../../assets/ticket-tracker.jpg";
 const MinorProjectData = [
   {
     id: 1,
@@ -14,6 +15,14 @@ const MinorProjectData = [
     description:"MovieDux is a web application that allows users to search for movies, view details, and manage their favorite movies. It provides an intuitive interface for movie enthusiasts to explore and organize their movie preferences.",
     image: moviedux,
     link:"https://movie-dux-delta.vercel.app/"
+  },
+  {
+    id:3,
+    title: "Ticket Tracker",
+    description:
+      "Ticket Tracker is a web application that allows users to track and manage their tickets for various events, such as concerts, sports games, and theater performances. It provides a convenient way to keep all ticket information organized in one place.",
+    image: tickettracker,
+    link: "https://ticket-tracker-eight.vercel.app/",
   }
 ];
 
